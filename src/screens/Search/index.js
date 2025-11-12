c9ba747e62f582683df6db1e63dd6fb4d468cd14
Monika Haseja <monikahaseja@gmail.com>
2025-11-12 11:53:28 +0530

    Modify Search page for better recipe filter handling

--- src/screens/Search/index.js
+++ src/screens/Search/index.js
@@ -1,12 +1,46 @@
-import React from "react";
+import React, { useContext, useEffect, useState } from "react";
+import { FlatList } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
+import { RecipesContext } from "../../../App";
+import Card from "../../components/Card";
 import Input from "../../components/Input";
 import styles from "./styles";
 
 const Search = () => {
+    const { recipes } = useContext(RecipesContext);
+    const [filteredRecipes, setFilteredRecipes] = useState([]);
+    const [keyword, setKeyword] = useState('');
+
+    useEffect(() => {
+        if (keyword?.length > 2) {
+            const filteredItems = recipes?.filter(rec => rec?.name?.toLowerCase()?.includes(keyword?.toLowerCase()))
+            setFilteredRecipes(filteredItems);
+        } else {
+            setFilteredRecipes([]);
+        }
+    }, [keyword])
+
     return (
         <SafeAreaView style={styles.container}>
-            <Input placeholder='Search Recipe' showSearchIcon={'../../../assets/search.png'}/>
+            <Input autoFocus placeholder='Search Recipe' showSearchIcon={'../../../assets/search.png'} onChangeText={setKeyword} value={keyword} />
+
+            <FlatList
+                data={filteredRecipes}
+                numColumns={2}
+                style={{ flexGrow: 1 }}
+                keyExtractor={item => String(item?.id)}
+                renderItem={({ item, index }) => (
+                    <Card
+                        title={item?.name}
+                        servings={item?.num_servings}
+                        image={item?.thumbnail_url}
+                        rating={item?.user_ratings?.score}
+                        author={item?.credits?.length
+                            ? { name: item?.credits[0]?.name, image: item?.credits[0]?.image_url }
+                            : null}
+                    />
+                )}
+            />
         </SafeAreaView>
     )
 }
